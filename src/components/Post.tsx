b5a238c311c35adb5ce389b8e9bab1b9c7b55890
kameoman <spiderman.935963@gmail.com>
2021-08-30 13:41:11 +0900

    posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -20,11 +20,26 @@ interface PROPS {
 }
 
 const Post: React.FC<PROPS>= (props) => {
-  return <div className={styles.post}>
+  return (
+  <div className={styles.post}>
     <div className={styles.post_avatar}>
       <Avatar src={props.avatar}/>
     </div>
-  </div>;
+    <div className={styles.body}>
+      <div>
+        <div className={styles.post_header}>
+          <h3>
+            <span className={styles.post_headerUser}>@{props.username}</span>
+            <span className={styles.post_headerTime}>{new Date(props.timestamp?.toDate()).toLocaleString()}</span>
+          </h3>
+        </div>
+        <div className={styles.post_tweet}>
+          <p>{props.text}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default Post;
